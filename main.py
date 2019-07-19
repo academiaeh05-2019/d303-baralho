5c4840e5956eef96288386647a0bbcbacd7356f9
Bruno Pimentel <brunoapimentel@gmail.com>
2019-07-19 11:49:09 -0300

    Preenche o baralho e cria método de sorteio

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from classes import Carta, Baralho
 
+baralho = Baralho() 
 
-carta1 = Carta('Paus', 'As', 1)
-carta2 = Carta('Copas', 'Rei', 10)
-carta3 = Carta('Espadas', 'Cinco', 5)
-baralho = Baralho()
-
-print(baralho)
-baralho.adicionar_carta(carta1)
-baralho.adicionar_carta(carta2)
-baralho.adicionar_carta(carta3)
-print(baralho)
-print(baralho.remover_carta())
-
+print(baralho.sortear())
+print(baralho.sortear())
+print(baralho.sortear())
